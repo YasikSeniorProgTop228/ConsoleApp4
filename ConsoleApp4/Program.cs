3a91f8fc98481462e773bf8d415c556a0829e893
YasikSeniorProgTop228 <lugovijaroslav9@gmail.com>
2024-09-24 20:36:22 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть математичний вираз: ");
+        string input = Console.ReadLine();
+
+        try
+        {
+            // Використання DataTable для обчислення виразу
+            var result = new DataTable().Compute(input, null);
+            Console.WriteLine($"Відповідь програми: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Сталася помилка: {ex.Message}");
+        }
+    }
+}
